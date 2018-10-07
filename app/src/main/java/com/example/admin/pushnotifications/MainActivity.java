6ec5a048fa42c6193272d0d45efabf286547cc82
Dina Moustafa <dina.moustafa15@gmail.com>
2018-10-07 12:12:41 +0200

    Test commit

--- app/src/main/java/com/example/admin/pushnotifications/MainActivity.java
+++ app/src/main/java/com/example/admin/pushnotifications/MainActivity.java
@@ -14,28 +14,33 @@ import android.view.View;
 import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Context mCtx = this;
-    Button mNotifyButton;
+    Button mNotifyButtonOne;
+    Button mNotifyButtonTwo;
+    int numberOfMessages=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mNotifyButton = findViewById(R.id.notify_button);
-        mNotifyButton.setOnClickListener(new View.OnClickListener() {
+        mNotifyButtonOne = findViewById(R.id.notify_button_one);
+        mNotifyButtonTwo = findViewById(R.id.notify_button_two);
+        mNotifyButtonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 pushNotification();
-
             }
         });
 
 
+        mNotifyButtonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+
     }
-
-
-    private void pushNotification(){
-
+    //Type 1: Normal View Style.
+    private void pushNotification() {
         //STEP 1: Create Notification Builder
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mCtx, "ChannelId_01");
         //STEP 2: Setting Notification Properties
@@ -45,16 +50,15 @@ public class MainActivity extends AppCompatActivity {
         mBuilder.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + mCtx.getPackageName() + "/" + R.raw.sound));
         mBuilder.setAutoCancel(true);
         //STEP 3: Attach Actions
-        Intent intent= new Intent(this,MainActivity.class);
-        TaskStackBuilder stackBuilder=TaskStackBuilder.create(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(MainActivity.class);
         // Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(intent);
-        Intent newIntent = new Intent(this,ResultIntent.class);
+        Intent newIntent = new Intent(this, ResultIntent.class);
         stackBuilder.addNextIntent(newIntent);
-        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(pendingIntent);
-
         //STEP 4: Issue the notification
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         //Making Screen LIGHT UP when receiving notifications.
@@ -63,9 +67,51 @@ public class MainActivity extends AppCompatActivity {
         PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "Tag");
         wakeLock.acquire();
         wakeLock.release();
-
-
-
     }
+    //Types 2: Big View Style.
+    private void displayNotification() {
+        //STEP 1: Create Notification Builder
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "channelId-02")
+                //STEP 2: Setting Notification Properties
+                .setContentTitle("Push Notifications")
+                .setContentText("You have received a new message.")
+                .setTicker("New Message Alert!")
+                .setAutoCancel(true)
+                .setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+getPackageName()+"/"+R.raw.sound))
+                .setSmallIcon(R.drawable.ic_launcher_background)
+        /* Increase notification number every time a new notification arrives */
+        .setNumber(++numberOfMessages);
 
+        /* Add Big View Specific Configuration */
+        NotificationCompat.InboxStyle mInboxStyle = new NotificationCompat.InboxStyle();
+        String[] events = new String[6];
+        events[0] = new String("This is first line....");
+        events[1] = new String("This is second line...");
+        events[2] = new String("This is third line...");
+        events[3] = new String("This is 4th line...");
+        events[4] = new String("This is 5th line...");
+        events[5] = new String("This is 6th line...");
+
+        // Sets a title for the Inbox style big view
+        mInboxStyle.setBigContentTitle("Big Title Details:");
+        for(int i=0;i<events.length;i++){
+            mInboxStyle.addLine(events[i]);
+
+
+        }
+        mBuilder.setStyle(mInboxStyle);
+
+
+        /* Creates an explicit intent for an Activity in your app */
+        Intent intent = new Intent(this,MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+
+        /* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0,mBuilder.build());
+    }
 }
